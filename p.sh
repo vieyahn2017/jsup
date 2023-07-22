19b4d16551b373a6c4036b5026204365732dca2f
vieyahn <vieyahn@163.com>
2023-07-22 10:50:22 +0800

    push.sh getopt

--- p.sh
+++ p.sh
@@ -1,4 +1,8 @@
 git add .
 
-git commit -m "$1"
-
+if [ -z "$1" ]; then
+  now=$(date "+%Y-%m-%d %H:%M:%S")
+  git commit -m "auto commit $now"
+else
+  git commit -m "$1"
+fi
--- push.sh
+++ push.sh
@@ -1,8 +1,37 @@
-git add .
+#!/bin/bash
 
-git commit -m "$1"
+function commit () {
+  git add .
+  if [ -z "$1" ]; then
+    now=$(date "+%Y-%m-%d %H:%M:%S")
+    git commit -m "auto commit $now"
+  else
+    git commit -m "$1"
+  fi
+}
 
-if [ "$2" ]; then
-  echo "$2"
-  git push -u origin master
-fi
+function push () {
+  if [ -z "$1" ]; then
+    git push -u origin master
+  else
+    git push -u origin master:$1
+  fi
+}
+
+echo "usage: sh push.sh -m msg -b branch"
+while getopts "m:b:" opt
+do
+  case $opt in
+    m)
+    echo "m parameter means commit messages: $OPTARG"
+    param_m=$OPTARG
+    ;;
+    b)
+    echo "b parameter means branch:$OPTARG"
+    param_b=$OPTARG
+    ;;
+  esac
+done
+
+commit $param_m
+push $param_b
